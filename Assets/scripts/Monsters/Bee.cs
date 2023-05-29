536be375f40887678a6297ed4d6541628005c2a6
상해꽃 이 <mxj1234@naver.com>
2023-05-30 03:20:34 +0900

    상태패턴

--- Assets/scripts/Monsters/Bee.cs
+++ Assets/scripts/Monsters/Bee.cs
@@ -1,13 +1,15 @@
+using BeeState;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class Bee : MonoBehaviour
 {
-     public enum State { Idle, Trace, Return, Attack, patrol, Size}  // 사이즈를 맨 마지막에 두면 열거형 크기를 알수있게끔 만들어둔다
-     public float detectRange;
+    public enum State { Idle, Trace, Return, Attack, patrol, Size }  // 사이즈를 맨 마지막에 두면 열거형 크기를 알수있게끔 만들어둔다
+    public float detectRange;
      public float attackRange;
      public float movespeed;
      public Transform[] PatroPoints;
@@ -17,15 +19,19 @@ public class Bee : MonoBehaviour
     public Transform player;
 
     private State curState;
-    private StateBase<Bee>[] states;
+    private StateBase[] states;
 
     private void Awake()
     {
-        states = new StateBase<Bee>[(int)State.Size];
+        states = new StateBase[(int)State.Size];
+        states[(int)State.Idle] = new IdleState(this);
+        states[(int)State.Trace] = new TraceState();
+        states[(int)State.Return] = new ReturnState();
+        states[(int)State.Attack] = new AttackState();
     }
     private void Start()
     {
-      
+        curState = State.Idle;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         returnPosition = transform.position;
     }
@@ -36,76 +42,77 @@ public class Bee : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
     }
-    public void ChangeState(StateBase<Bee> state)
+    private void Update()
     {
-
+        states[(int)curState].Update();
+    }
+    public void ChangeState(State state)
+    {
+        curState = state;
     }
 }
 
 namespace BeeState
 {
    
-    public class IdleState : StateBase<Bee>
+    public class IdleState : StateBase
     {
+        private Bee bee;
         private float idleTime;
-        public IdleState(Bee owner) : base(owner)
+        public IdleState(Bee bee)
         {
-        }
-        public override void Enter()
-        {
-            idleTime = 0;
-        }
+            this.bee = bee;
 
-        public override void Exit()
-        {
-           
         }
-
-        public override void Setup()
-        {
-            
-        }
-
+     
         public override void Update()
         {
             idleTime += Time.deltaTime;
             if (idleTime > 2)
             {
                 idleTime = 0;
-                owner.patrolIndex = (owner.patrolIndex + 1) % owner.PatroPoints.Length;
-                //curState = State.patrol;
+                bee.patrolIndex = (bee.patrolIndex + 1) % bee.PatroPoints.Length;
+                bee.ChangeState(Bee.State.patrol);
             }
 
             // 플레이어와 가까워지면 다가가기
-            else if (Vector2.Distance(owner.player.position, owner.transform.position) < owner.detectRange)
+            else if (Vector2.Distance(bee.player.position, bee.transform.position) < bee.detectRange)
             {
-                //curState = State.Trace;
+                bee.ChangeState(Bee.State.Trace);
             }
+
         }
     }
-    public class TraceState : StateBase<Bee>
+    public class TraceState : StateBase
     {
-        public TraceState(Bee owner) : base(owner)
-        {
-        }
-        public override void Enter()
-        {
-
-        }
-
-        public override void Exit()
-        {
-
-        }
-
-        public override void Setup()
-        {
-
-        }
-
+        private Bee bee;
         public override void Update()
         {
 
+        }
+    }
+    public class ReturnState : StateBase
+    {
+        private Bee bee;
+        public override void Update()
+        {
+            
+        }
+    }
+    public class AttackState : StateBase
+    {
+        private Bee bee;
+        public override void Update()
+        {
+            
+        }
+    }
+    public class PatrolState : StateBase
+    {
+        private Bee bee;
+        public override void Update()
+        {
+           
         }
     }
 }
